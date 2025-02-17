d0c2c02180337d810a6442fff5e67f49a62ae4e7
Hai Duong <149478749+HaixD@users.noreply.github.com>
2025-02-17 03:11:11 -0800

    remove testing environment stuff

--- src/main.js
+++ src/main.js
@@ -17,65 +17,7 @@ async function main() {
     GameEngine.init(ctx);
     ctx.imageSmoothingEnabled = false;
 
-    // set up scene #1
-    GameEngine.createScene("scene1", scene => {
-        const player = new Player(new InstanceVector());
-        const camera = new InterpolatedCamera(
-            player.position,
-            5,
-            0,
-            new Vector(-canvas.width / 2, -canvas.height / 2).add(
-                player.getBoundary().asShape().multiply(0.5)
-            )
-        );
-
-        scene.setOffset(camera.position);
-        scene.addLayer("FAKE_LAYER");
-        scene.addLayer("back");
-        scene.addLayer("middle");
-        scene.addLayer("front");
-
-        scene.addGameObject("FAKE_LAYER", camera);
-        scene.addGameObject(
-            "front",
-            new Obstacle(new InstanceVector(1000, -1000), new Vector(20, 1200))
-        );
-
-        scene.addGameObject("front", new Obstacle(new InstanceVector(0, 100), new Vector(50, 50)));
-        scene.addGameObject("front", new Obstacle(new InstanceVector(-50, 0), new Vector(50, 50)));
-
-        for (let i = 0; i < 10; ++i) {
-            scene.addGameObject(
-                "front",
-                new Stair(
-                    new InstanceVector(-700 - 40 * i, 200 - 20 * i),
-                    new Vector(40, 20),
-                    Stair.DIRECTION.LEFT
-                )
-            );
-            scene.addGameObject(
-                "front",
-                new Stair(
-                    new InstanceVector(-2000 - 40 * i, 200 - 40 * i),
-                    new Vector(40, 40),
-                    Stair.DIRECTION.LEFT
-                )
-            );
-        }
-
-        scene.addGameObject(
-            "front",
-            new Obstacle(new InstanceVector(-2000, 200), new Vector(4000, 20))
-        );
-        scene.addGameObject(
-            "front",
-            new Obstacle(new InstanceVector(-2000, -1000), new Vector(4000, 20))
-        );
-        scene.addGameObject("back", player);
-    });
-
-    // set up scene #2
-    GameEngine.createScene("scene2", scene => {
+    GameEngine.createScene("main", scene => {
         const player = new Player(new InstanceVector());
         const camera = new InterpolatedCamera(
             player.position,
--- src/player.js
+++ src/player.js
@@ -4,7 +4,6 @@
 /** @typedef {import("./engine/util")} */
 /** @typedef {import("./engine/assetmanager")} */
 /** @typedef {import("./engine/gameengine")} */
-/** @typedef {import("./stair-controller")} */
 
 class Player extends GameObject {
     static TYPE_ID = Symbol(Player.name);
@@ -21,35 +20,12 @@ class Player extends GameObject {
         this.position = position;
 
         const playerShape = Player.#shape.multiply(Player.#scale);
-        const legHeight = playerShape.y / 2;
-        const snapHeight = 40;
-        this.topCollider = new ColliderRect(
+        this.collider = new ColliderRect(
             this,
             this.position,
             new Vector(),
-            new Vector(playerShape.x, playerShape.y - legHeight),
-            Obstacle.TYPE_ID,
-            Stair.TYPE_ID
-        );
-        this.middleCollider = new ColliderRect(
-            this,
-            this.position,
-            new Vector(0, playerShape.y - legHeight),
-            new Vector(playerShape.x, legHeight),
-            Obstacle.TYPE_ID,
-            Stair.TYPE_ID
-        );
-        this.bottomCollider = new ColliderRect(
-            this,
-            this.position,
-            new Vector(0, playerShape.y),
-            new Vector(playerShape.x, snapHeight),
-            Stair.TYPE_ID
-        );
-        this.stairController = new StairController(
-            this.middleCollider,
-            this.bottomCollider,
-            Infinity
+            new Vector(playerShape.x, playerShape.y),
+            Obstacle.TYPE_ID
         );
         this.controller = new FallingPlayerController(
             new Vector(750, Infinity),
@@ -69,7 +45,7 @@ class Player extends GameObject {
             run: new Spritesheet(
                 AssetManager.getImage("/anims/placeholder-walk.png"),
                 new Vector(),
-                new Vector(12, 24),
+                Player.#shape,
                 Player.#scale,
                 4,
                 0.5
@@ -115,18 +91,15 @@ class Player extends GameObject {
         );
         this.position.add(displacement);
 
-        const topAdjustment = this.topCollider.resolveCollisions(displacement);
-        this.position.add(topAdjustment);
-
-        const stairAdjustment = this.stairController.updateState(displacement);
-        this.position.add(stairAdjustment);
+        const adjustment = this.collider.resolveCollisions(displacement);
+        this.position.add(adjustment);
 
         // compute last blocked directions with a 1 update delay (so things match up visually)
         this.lastBlockedDirections = table.NO_BLOCK;
-        this.lastBlockedDirections |= table.LEFT * (topAdjustment.x > 0 || stairAdjustment.x > 0);
-        this.lastBlockedDirections |= table.RIGHT * (topAdjustment.x < 0 || stairAdjustment.x < 0);
-        this.lastBlockedDirections |= table.ABOVE * (topAdjustment.y > 0);
-        this.lastBlockedDirections |= table.BELOW * (stairAdjustment.y < 0);
+        this.lastBlockedDirections |= table.LEFT * (adjustment.x > 0);
+        this.lastBlockedDirections |= table.RIGHT * (adjustment.x < 0);
+        this.lastBlockedDirections |= table.ABOVE * (adjustment.y > 0);
+        this.lastBlockedDirections |= table.BELOW * (adjustment.y < 0);
 
         this.sprite.incrementTimeline(deltaTime);
         this.sprite.setHorizontalFlip(this.controller.velocity.x < 0);
@@ -144,10 +117,8 @@ class Player extends GameObject {
         this.sprite.drawSprite(ctx);
 
         // debugging
-        this.topCollider.drawCollider(ctx);
-        this.middleCollider.drawCollider(ctx);
+        this.collider.drawCollider(ctx);
         this.sprite.drawOutline(ctx);
-        this.bottomCollider.drawCollider(ctx);
     }
 
     /**
@@ -157,7 +128,7 @@ class Player extends GameObject {
         if (events.leftClick !== null) {
             const difference = this.position
                 .asVector()
-                .add(this.topCollider.shape.multiply(0.5))
+                .add(this.collider.shape.multiply(0.5))
                 .subtract(events.worldMousePosition)
                 .multiply(5);
             this.controller.velocity.add(difference);
